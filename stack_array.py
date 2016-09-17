8934c5d0eee82231214ae2bb6fb8c55e09e14cd0
Vikas Verma <vikk2411@gmail.com>
2016-09-18 00:51:06 +0530

    Simple Stack using Array(list) Implementation

---
+++ stack_array.py
@@ -0,0 +1,56 @@
+## Stack Implementation using Arrays
+# We know arrays as Lists in python, but we will use Array word only. Dont confuse.
+
+class Stack(object):
+	"""docstring for Stack"""
+	def __init__(self, limit, array = []):
+		self.array = array
+		self.limit = limit
+		self.top = None
+		self.size = len(self.array)
+		self.set_top()
+		self.pprint()
+
+	def set_top(self):
+		if self.array == []:
+			self.top = None
+		else:
+			self.top = self.array[-1]
+
+	def is_full(self):
+		return self.size == self.limit
+
+	def is_empty(self):
+		return self.size == 0			
+
+	def push(self, value):
+		if self.size == self.limit:
+			print "Stack Overflow"
+		else:
+			self.array.append(value)
+			self.set_top()
+			self.size += 1
+			self.pprint()
+
+	def pop(self):
+		if self.size == 0:
+			print "Stack Underflow"
+		else:	
+			print "Removing the item " + str(self.top)
+			self.array.pop()
+			self.set_top()
+			self.size -= 1
+			self.pprint()
+
+	def pprint(self):                         # could have used self.array.reverse() for the same thing
+		if 	self.is_empty == True:
+			print "Stack Empty"
+		else:	
+			print "-------------> Top <------------"
+			i= 0
+			while i < len(self.array):
+				i+=1
+				print "|              ^"	+ str(self.array[i*-1]) + "             |"
+			print "================================"
+
+
